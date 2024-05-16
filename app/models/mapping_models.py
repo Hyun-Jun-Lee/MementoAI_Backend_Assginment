9b7e6abcfda566c980b03712430d33d305d14755
Lee Hyun Jun <bhk0827@gmail.com>
2024-05-16 12:28:41 +0900

    <Fix> : remove unique option in origin url

--- app/models/mapping_models.py
+++ app/models/mapping_models.py
@@ -7,7 +7,7 @@ from db.base import ModelBase
 class URLMapping(ModelBase):
     __tablename__ = "url_mapping"
 
-    origin_url = Column(String, unique=True)
+    origin_url = Column(String)
     shorten_key = Column(String, unique=True)
     view_count = Column(Integer, default=0)
     expire_date = Column(DATE, nullable=True)
