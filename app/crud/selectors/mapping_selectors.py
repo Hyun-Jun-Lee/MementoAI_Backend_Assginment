000aa00f0e00d9f8b025989b559bd51b341e824a
Lee Hyun Jun <bhk0827@gmail.com>
2024-05-16 15:53:07 +0900

    <Fix> : change function based to class based

--- app/crud/selectors/mapping_selectors.py
+++ app/crud/selectors/mapping_selectors.py
@@ -1,23 +1,22 @@
 from datetime import datetime
 from sqlalchemy.orm import Session
 
-from models import mapping_models
 from exception.exceptions import ExpiredException
+from models import mapping_models
+from crud.base import BaseSelector
 
 
-def get_url_by_key(db: Session, key: str):
-    return (
-        db.query(mapping_models.URLMapping)
-        .filter(mapping_models.URLMapping.shorten_key == key)
-        .first()
-    )
+class UrlMappingSelector(BaseSelector):
+
+    def get_url_by_key(self, db: Session, key: str):
+        return db.query(self.model).filter(self.model.shorten_key == key).first()
+
+    def get_url_mapping(self, db: Session, url_mapping: mapping_models.URLMapping):
+
+        if url_mapping.expire_date and url_mapping.expire_date < datetime.now().date():
+            raise ExpiredException
+
+        return url_mapping
 
 
-def get_url_mapping(db: Session, url_mapping: mapping_models.URLMapping):
-
-    if url_mapping.expire_date and url_mapping.expire_date < datetime.now().date():
-        db.delete(url_mapping)
-        db.commit()
-        raise ExpiredException
-
-    return url_mapping
+mapping_selector = UrlMappingSelector(mapping_models.URLMapping)
--- app/crud/services/mapping_services.py
+++ app/crud/services/mapping_services.py
@@ -1,47 +1,30 @@
-import secrets
-import hashlib
 from datetime import date
 from sqlalchemy.orm import Session
 
+from crud.base import BaseService
 from models import mapping_models
-from crud.selectors import mapping_selectors
+from crud.utilities import create_random_key
 
 
-def generate_url_hash(origin_url: str) -> str:
-    return hashlib.sha256(origin_url.encode()).hexdigest()
+class UrlMappingService(BaseService):
+
+    def create_mapping(self, db: Session, origin_url: str, expire_date: date = None):
+        random_key = create_random_key(db=db, origin_url=origin_url)
+
+        url_mapping = mapping_models.URLMapping(
+            origin_url=origin_url, shorten_key=random_key, expire_date=expire_date
+        )
+
+        db.add(url_mapping)
+        db.commit()
+        return url_mapping
+
+    def increase_view_count(self, db: Session, url_mapping: mapping_models.URLMapping):
+        url_mapping.view_count += 1
+        db.commit()
+        db.refresh(url_mapping)
+
+        return url_mapping
 
 
-def generate_random_key():
-    return secrets.token_urlsafe(5)
-
-
-def create_random_key(db: Session, origin_url: str) -> str:
-    url_hash = generate_url_hash(origin_url)[:5]
-    random_key = generate_random_key()
-    short_key = f"{random_key}_{url_hash}"
-
-    while mapping_selectors.get_url_by_key(db, short_key):
-        random_key = generate_random_key()
-        short_key = f"{random_key}_{url_hash}"
-
-    return short_key
-
-
-def create_mapping(db: Session, origin_url: str, expire_date: date = None):
-    random_key = create_random_key(db=db, origin_url=origin_url)
-
-    url_mapping = mapping_models.URLMapping(
-        origin_url=origin_url, shorten_key=random_key, expire_date=expire_date
-    )
-
-    db.add(url_mapping)
-    db.commit()
-    return url_mapping
-
-
-def increase_view_count(db: Session, url_mapping: mapping_models.URLMapping):
-    url_mapping.view_count += 1
-    db.commit()
-    db.refresh(url_mapping)
-
-    return url_mapping
+mapping_service = UrlMappingService(mapping_models.URLMapping)
