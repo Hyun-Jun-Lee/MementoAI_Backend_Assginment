581c833e3055453c1039384ce9b3e5e1b258ddb5
Lee Hyun Jun <bhk0827@gmail.com>
2024-05-16 15:57:12 +0900

    <Feat> : Add Read, Update endpoint & no cache
    - GET /shorten_key 에서 301 status code를 받아도 클라이언트에서 cache에 저장하지 않도록
    	- 그래야 조회수 증가됨
    - 편리한 작동 테스트 위한 GET /shortens, PATCH /shorten/shorten_key 추가

--- app/api/v1/url_mapping.py
+++ app/api/v1/url_mapping.py
@@ -1,18 +1,45 @@
 from fastapi import APIRouter, Depends, HTTPException
-from fastapi.responses import RedirectResponse
+from fastapi.responses import RedirectResponse, Response
 from sqlalchemy.orm import Session
+from typing import List
 
 from db.session import get_db
-from schemas.mapping_schemas import UrlMappingCreate, UrlMappingResponse
+from schemas.mapping_schemas import (
+    UrlMappingCreate,
+    UrlMappingResponse,
+    UrlMappingUpdate,
+)
 from exception.exceptions import ExpiredException
-from crud.services import mapping_services
-from crud.selectors import mapping_selectors
+from crud import mapping_selector, mapping_service
 
 router = APIRouter()
 
 
+@router.get("/shortens", response_model=List[UrlMappingResponse])
+def get_all_mappings(db: Session = Depends(get_db)):
+    url_mappings = mapping_selector.get_multi(db=db)
+
+    return url_mappings
+
+
+@router.patch("/shorten/{shorten_key}", response_model=UrlMappingResponse)
+def update_mapping(
+    shorten_key: str, update_in: UrlMappingUpdate, db: Session = Depends(get_db)
+):
+    check_url_mapping = mapping_selector.get_url_by_key(db=db, key=shorten_key)
+
+    if not check_url_mapping:
+        raise HTTPException(status_code=404, detail="URL Not Found")
+
+    updated_url_mapping = mapping_service.update(
+        db=db, db_obj=check_url_mapping, obj_in=update_in
+    )
+
+    return updated_url_mapping
+
+
 @router.post("/shorten", response_model=UrlMappingResponse)
-def create_short_key(url_in: UrlMappingCreate, db=Depends(get_db)):
+def create_short_key(url_in: UrlMappingCreate, db: Session = Depends(get_db)):
     """
     원본 URL 받아서 단축 Key를 반환
 
@@ -22,7 +49,7 @@ def create_short_key(url_in: UrlMappingCreate, db=Depends(get_db)):
     - Returns:
         - UrlMappingResponse: 원본 URL, 단축 Key, 만료 날짜, 조회수를 포함하는 응답 모델
     """
-    url_mapping = mapping_services.create_mapping(
+    url_mapping = mapping_service.create_mapping(
         db=db, origin_url=url_in.origin_url, expire_date=url_in.expire_date
     )
 
@@ -30,9 +57,9 @@ def create_short_key(url_in: UrlMappingCreate, db=Depends(get_db)):
 
 
 @router.get("/{shorten_key}", response_model=UrlMappingResponse)
-def get_origin_url(shorten_key: str, db=Depends(get_db)):
+def get_origin_url(shorten_key: str, db: Session = Depends(get_db)):
     """
-    단축 Key를 받아서 원본 URL 반환
+    단축 Key를 받아서 원본 URL로 리다이렉트
 
     - Parameters:
         - shorten_key : 단축 Key
@@ -40,25 +67,30 @@ def get_origin_url(shorten_key: str, db=Depends(get_db)):
     - Returns:
         - UrlMappingResponse: 원본 URL, 단축 Key, 만료 날짜, 조회수를 포함하는 응답 모델
     """
-    check_url_mapping = mapping_selectors.get_url_by_key(db=db, key=shorten_key)
+    check_url_mapping = mapping_selector.get_url_by_key(db=db, key=shorten_key)
 
     if not check_url_mapping:
         raise HTTPException(status_code=404, detail="URL Not Found")
+
     try:
-        url_mapping = mapping_selectors.get_url_mapping(
+        url_mapping = mapping_selector.get_url_mapping(
             db=db, url_mapping=check_url_mapping
         )
-        mapping_services.increase_view_count(db=db, url_mapping=url_mapping)
+        mapping_service.increase_view_count(db=db, url_mapping=url_mapping)
     except ExpiredException:
+        mapping_service.delete(db=db, id=check_url_mapping.id)
         raise HTTPException(status_code=404, detail="URL has expired")
 
-    return RedirectResponse(url=url_mapping.origin_url, status_code=301)
+    headers = {"Cache-Control": "no-cache, no-store, pragma: no-cache"}
+    return RedirectResponse(
+        url=url_mapping.origin_url, headers=headers, status_code=301
+    )
 
 
 @router.get("/stats/{shorten_key}", response_model=UrlMappingResponse)
-def get_view_count(shorten_key: str, db=Depends(get_db)):
+def get_view_count(shorten_key: str, db: Session = Depends(get_db)):
     """
-    단축 Key 받아서 원본 URL 반환
+    단축 Key 받아서 해당 key의 조회 수 확인
 
     - Parameters:
         - shorten_key : 단축 Key
@@ -67,7 +99,7 @@ def get_view_count(shorten_key: str, db=Depends(get_db)):
         - UrlMappingResponse: 원본 URL, 단축 URL, 만료 날짜, 조회수를 포함하는 응답 모델
     """
 
-    url_mapping = mapping_selectors.get_url_by_key(db=db, key=shorten_key)
+    url_mapping = mapping_selector.get_url_by_key(db=db, key=shorten_key)
 
     if not url_mapping:
         raise HTTPException(status_code=404, detail="URL Not Found")
